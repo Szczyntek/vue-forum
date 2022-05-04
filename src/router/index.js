cd426ed152ca5adc9957442c3c1e46e3a13b99d6
Dominik Wegrzynowski <szczyntek@gmail.com>
2022-05-04 21:16:16 +0200

    Added styles & router

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,22 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import PageHome from '@/components/PageHome'
+import { createApp } from 'vue'
+import App from '../App.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Home',
+    component: PageHome
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
-
+const forumApp = createApp(App)
+forumApp.use(router)
+forumApp.mount('#app')
 export default router
